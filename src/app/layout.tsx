289eab0513e8bda37b45b9b53fc1acab230cd239
Sktamalik_ <andisaktamalik@gmail.com>
2026-07-22 16:44:06 +0800

    add favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export const metadata: Metadata = {
   robots: { index: true, follow: true, googleBot: { index: true, follow: true, "max-video-preview": -1, "max-image-preview": "large", "max-snippet": -1 } },
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/Favicon.png", type: "image/png" },
       { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
@@ -38,7 +39,7 @@ export const metadata: Metadata = {
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
-    shortcut: "/Favicon.png",
+    shortcut: "/favicon.ico",
   },
   openGraph: {
     title: TITLE, description: DESCRIPTION, url: SITE_URL, siteName: "LihatLangit",
